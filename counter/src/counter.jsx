bcf12597c5ab4f3797954fc0faf3b095d77c02fe
Rohit <rohhit.rz@gmail.com>
2024-08-21 17:19:01 +0530

    commit

---
+++ counter/src/counter.jsx
@@ -0,0 +1,25 @@
+import React ,{useState} from "react";
+
+const Counter =()=>{
+    const[count,setCount]=useState(0);
+
+    const increment=()=>setCount(count+1);
+    const decrement=()=>setCount(count-1);
+
+    return (
+<>
+        <h1>Counter App</h1>
+        <p>Count: {count}</p>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+
+        </>
+
+    )
+
+
+
+}
+
+export default Counter
+
